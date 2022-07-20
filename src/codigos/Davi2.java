064c3eb0890374c6b96cde6e361f19d9842403cd
Davi Martins <davimartinsfreitas@gmail.com>
2022-07-20 10:10:13 -0300

    commit na branch test

--- src/codigos/Davi2.java
+++ src/codigos/Davi2.java
@@ -5,7 +5,7 @@ public class Davi2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for (int i = 0; i <= 50; i+=2) {
-			System.out.println(i + " ");
+			System.out.println(i);
 		}
 	}
 
