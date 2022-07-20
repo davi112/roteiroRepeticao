255c1d3a42fa607c83c5e56817e05de561b6b97e
Davi Martins <davimartinsfreitas@gmail.com>
2022-07-20 10:05:40 -0300

    changing 2 and adding 9

--- src/codigos/Davi9.java
+++ src/codigos/Davi9.java
@@ -1,12 +1,10 @@
 package codigos;
 
-public class Davi2 {
+public class Davi9 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (int i = 0; i <= 50; i+=2) {
-			System.out.println(i + " ");
-		}
+		
 	}
 
 }
